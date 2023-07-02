2d0ac15b8cede704e3b5d8f2f016a09f4f537a7a
Aamir Sohail <80810119+Aamir-Sohail772@users.noreply.github.com>
2023-07-02 19:19:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,25 @@ class App extends React.Component{
     this.handleClick = this.handleClick.bind(this)
   }
 
+  shouldComponentUpdate(nextProps, nextState){
+    return nextState.count % 2 === 0;
+  }
 
   render(){
     console.log(`Rendering with count:-${this.state.count}`)
+    
+    if(this.state.count % 2 === 0 {
+      
     return(
       <div>
         <span id="count">{this.state.count}</span>
         <button id="incr-btn" onClick={this.handleClick}>Increment</button>
       </div>
     )
-
+    
+  }
+    return null;
+  
   }
 }
 
